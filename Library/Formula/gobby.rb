e4b9c19846e6e4b0f249199dec5018fe25f579fd
Marnen Laibow-Koser <marnen@marnen.org>
2011-06-09 13:17:35 -0400

    Removing cmake lines.

--- Library/Formula/gobby.rb
+++ Library/Formula/gobby.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class Gobby < Formula
   url 'http://releases.0x539.de/gobby/gobby-0.4.12.tar.gz'
+  head 'git://git.0x539.de/git/gobby.git'
   homepage 'http://gobby.0x539.de'
   md5 '835cc48f5177196e4a18610c2cb013bf'
-
-  # depends_on 'cmake'
+  
+  depends_on 'gtkmm'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    # system "cmake . #{std_cmake_parameters}"
     system "make install"
   end
 end
