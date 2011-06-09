80ea40f72d5b392e6d264544d52912c55f918f0f
Marnen Laibow-Koser <marnen@marnen.org>
2011-06-09 12:56:45 -0400

    Specify a particular version and MD5 sum.

--- Library/Formula/gobby.rb
+++ Library/Formula/gobby.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
 class Gobby < Formula
-  url 'git://git.0x539.de/git/gobby.git'
+  url 'http://git.0x539.de/?p=gobby.git;a=snapshot;h=44eeb15f37d732cea9dc41fc72852c3231570ab0;sf=tgz'
+  version '0.4.94'
   homepage 'http://gobby.0x539.de'
-  md5 ''
+  md5 '8a3cd43db6bcee32c7e48063817916a0'
 
   # depends_on 'cmake'
 
