04c6fa5475e0878c8432d21c07dc3611de84c81a
Marnen Laibow-Koser <marnen@marnen.org>
2011-06-09 13:02:06 -0400

    Fix URL and add head.

--- Library/Formula/gobby.rb
+++ Library/Formula/gobby.rb
@@ -1,10 +1,9 @@
 require 'formula'
 
 class Gobby < Formula
-  url 'http://git.0x539.de/?p=gobby.git;a=snapshot;h=44eeb15f37d732cea9dc41fc72852c3231570ab0;sf=tgz'
-  version '0.4.94'
+  url 'http://releases.0x539.de/gobby/gobby-0.4.12.tar.gz'
   homepage 'http://gobby.0x539.de'
-  md5 '8a3cd43db6bcee32c7e48063817916a0'
+  md5 '835cc48f5177196e4a18610c2cb013bf'
 
   # depends_on 'cmake'
 
