8fe56aac404729cf1fad7aeca9baf749b5728ee7
Marnen Laibow-Koser <marnen@marnen.org>
2013-09-16 23:48:35 -0400

    Remove redundant version number.

--- Library/Formula/platypus.rb
+++ Library/Formula/platypus.rb
@@ -3,7 +3,6 @@ require 'formula'
 class Platypus < Formula
   homepage 'http://sveinbjorn.org/platypus'
   url 'https://github.com/sveinbjornt/Platypus/raw/4.8/Releases/platypus4.8.src.zip'
-  version '4.8'
   sha1 '39d165b9579600cef637b45c70c82307697bb7be'
   head 'https://github.com/sveinbjornt/Platypus', :branch => 'master'
 
