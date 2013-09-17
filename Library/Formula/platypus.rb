85fce796f5efbe9d861a32a9149b75e575318f1d
Marnen Laibow-Koser <marnen@marnen.org>
2013-09-16 23:44:00 -0400

    Update Platypus formula.

--- Library/Formula/platypus.rb
+++ Library/Formula/platypus.rb
@@ -2,20 +2,22 @@ require 'formula'
 
 class Platypus < Formula
   homepage 'http://sveinbjorn.org/platypus'
-  url 'https://github.com/pmenglund/Platypus/archive/v4.8.1.tar.gz'
-  sha256 '5723a8276bb9ed068149b48994da6f9ee581611eebf9228e0d2977e2eb505dbd'
-
+  url 'https://github.com/sveinbjornt/Platypus/raw/4.8/Releases/platypus4.8.src.zip'
+  version '4.8'
+  sha1 '39d165b9579600cef637b45c70c82307697bb7be'
   head 'https://github.com/sveinbjornt/Platypus', :branch => 'master'
 
   depends_on :xcode
 
   def install
-    system "xcodebuild", "SYMROOT=build",
-                         "-project", "Platypus.xcodeproj",
-                         "-target", "platypus",
-                         "clean", "install"
-    man1.install "CommandLineTool/platypus.1"
-    bin.install_p "build/UninstalledProducts/platypus_clt", "platypus"
+    Dir.chdir 'Platypus 4.8 Source' do
+      system "xcodebuild", "SYMROOT=build",
+                           "-project", "Platypus.xcodeproj",
+                           "-target", "platypus",
+                           "clean", "install"
+      man1.install "CommandLineTool/platypus.1"
+      bin.install "/tmp/Platypus.dst/platypus_clt" => "platypus"
+    end
   end
 
   def test
